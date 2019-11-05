709cc35a3d67e73e2b14bd3583ffc34c9a16d3e9
Aleksandar Stojadinovic <aleksandar.stojadinovic.3641@metropolitan.ac.rs>
2019-11-05 16:03:20 +0100

    Dodate CSS animacije sa jos nekim sitnim izmenama

--- src/bulletboard-frontend/src/app/home-slidehow/home-slidehow.component.ts
+++ src/bulletboard-frontend/src/app/home-slidehow/home-slidehow.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { MatStepper } from '@angular/material';
 
 @Component({
   selector: 'app-home-slidehow',
@@ -7,16 +6,15 @@ import { MatStepper } from '@angular/material';
   styleUrls: ['./home-slidehow.component.css']
 })
 export class HomeSlidehowComponent implements OnInit {
-  @ViewChild('stepper', { static: false }) stepper: MatStepper;
   slideTimer;
-  prevSlide;
+  slideIndex = 0;
 
   slides: any = [
     { slideID: 0, slideTitle: "Mnogo dobar title! #1", slideBodyText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent dignissim mattis enim, nec blandit augue tempor at. Proin eu iaculis velit. Quisque commodo mi in elit volutpat blandit non ut sem. ", slideImageUrl: "https://unsplash.it/1920/1080", poll: { questionID: 0, questionText: "Koliko puta pijes kafu dnevno?", questionOptions: [1, 2, 3, 4, 5] } },
-    { slideID: 2, slideTitle: "Mnogo dobar title! #2", slideBodyText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent dignissim mattis enim, nec blandit augue tempor at. Proin eu iaculis velit. Quisque commodo mi in elit volutpat blandit non ut sem. ", slideImageUrl: "https://unsplash.it/1920/1080", poll: { questionID: 1, questionText: "Koliko puta pijes kafu nedeljno?", questionOptions: [1, 2, 3, 4, 5] } },
-    { slideID: 3, slideTitle: "Mnogo dobar title! #3", slideBodyText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent dignissim mattis enim, nec blandit augue tempor at. Proin eu iaculis velit. Quisque commodo mi in elit volutpat blandit non ut sem. ", slideImageUrl: "https://unsplash.it/1920/1080", poll: { questionID: 2, questionText: "Koliko puta pijes kafu mesecno?", questionOptions: [1, 2, 3, 4, 5] } },
-    { slideID: 4, slideTitle: "Mnogo dobar title! #4", slideBodyText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent dignissim mattis enim, nec blandit augue tempor at. Proin eu iaculis velit. Quisque commodo mi in elit volutpat blandit non ut sem. ", slideImageUrl: "https://unsplash.it/1920/1080", poll: { questionID: 3, questionText: "Koliko puta pijes kafu godisnje?", questionOptions: [1, 2, 3, 4, 5] } },
-    { slideID: 5, slideTitle: "Mnogo dobar title! #5", slideBodyText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent dignissim mattis enim, nec blandit augue tempor at. Proin eu iaculis velit. Quisque commodo mi in elit volutpat blandit non ut sem. ", slideImageUrl: "https://unsplash.it/1920/1080", poll: { questionID: 4, questionText: "Koliko puta pijes kafu na sat?", questionOptions: [1, 2, 3, 4, 5] } }
+    { slideID: 1, slideTitle: "Mnogo dobar title! #2", slideBodyText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent dignissim mattis enim, nec blandit augue tempor at. Proin eu iaculis velit. Quisque commodo mi in elit volutpat blandit non ut sem. ", slideImageUrl: "https://unsplash.it/1920/1080", poll: { questionID: 1, questionText: "Koliko puta pijes kafu nedeljno?", questionOptions: [1, 2, 3, 4, 5] } },
+    { slideID: 2, slideTitle: "Mnogo dobar title! #3", slideBodyText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent dignissim mattis enim, nec blandit augue tempor at. Proin eu iaculis velit. Quisque commodo mi in elit volutpat blandit non ut sem. ", slideImageUrl: "https://unsplash.it/1920/1080", poll: { questionID: 2, questionText: "Koliko puta pijes kafu mesecno?", questionOptions: [1, 2, 3, 4, 5] } },
+    { slideID: 3, slideTitle: "Mnogo dobar title! #4", slideBodyText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent dignissim mattis enim, nec blandit augue tempor at. Proin eu iaculis velit. Quisque commodo mi in elit volutpat blandit non ut sem. ", slideImageUrl: "https://unsplash.it/1920/1080", poll: { questionID: 3, questionText: "Koliko puta pijes kafu godisnje?", questionOptions: [1, 2, 3, 4, 5] } },
+    { slideID: 4, slideTitle: "Mnogo dobar title! #5", slideBodyText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent dignissim mattis enim, nec blandit augue tempor at. Proin eu iaculis velit. Quisque commodo mi in elit volutpat blandit non ut sem. ", slideImageUrl: "https://unsplash.it/1920/1080", poll: { questionID: 4, questionText: "Koliko puta pijes kafu na sat?", questionOptions: [1, 2, 3, 4, 5] } }
   ]
 
   constructor() { }
@@ -27,15 +25,23 @@ export class HomeSlidehowComponent implements OnInit {
 
   startSlides() {
     this.slideTimer = setInterval(() => {
-      this.prevSlide = this.stepper.selectedIndex;
-      this.stepper.next();
-      if (this.prevSlide == this.stepper.selectedIndex)
-        this.stepper.selectedIndex = 0;
+      this.slideIndex++;
+      if (this.slideIndex >= this.slides.length)
+        this.slideIndex = 0;
+
+      console.log(this.slideIndex);
+
     }, 5000);
   }
 
   stopSlideTimer() {
     clearInterval(this.slideTimer);
+  }
+
+  goToSlide(index) {
+    this.slideIndex = index;
+    this.stopSlideTimer();
+    this.startSlides();
   }
 
   handleNumberInput(event) {
