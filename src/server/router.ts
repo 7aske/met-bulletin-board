765852b63df62ca89f78af0891f434f11968154d
Nikola Tasic <ntasic7@gmail.com>
2020-01-12 18:32:07 +0100

    index routing

--- src/server/router.ts
+++ src/server/router.ts
@@ -3,13 +3,13 @@ import slide from "./routes/slide";
 import vote from "./routes/vote";
 import poll from "./routes/poll";
 import auth from "./routes/auth";
+import index from "./routes";
 import { isDev } from "./utils/dev";
 import { authorizeLocalhost, authorizeRequest } from "./middleware/authentication";
 
 const router = Router();
 
 router.use("/auth", auth);
-
 if (isDev()) {
 	router.use("/vote", vote);
 	router.use("/poll", poll);
@@ -20,6 +20,6 @@ if (isDev()) {
 	router.use("/poll", authorizeRequest, poll);
 	router.use("/slide", authorizeRequest, slide);
 }
-
+router.use("/", index);
 
 export default router;
